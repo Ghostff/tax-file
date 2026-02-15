b608e322a37ebfeb83385993cca976f35ac92e50
ghostff <frankchris@hotmail.com>
2026-02-14 20:56:07 -0600

    base-working

--- frontend/.nuxt/dist/server/client.manifest.mjs
+++
@@ -1,4 +0,0 @@
-
-import { viteNodeFetch } from "file:///var/www/Projects/tax-file/frontend/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
-export default () => viteNodeFetch.getManifest()
-    
